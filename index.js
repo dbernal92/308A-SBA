99d79a765fac8b47fb0e10a41154e73f8153abaa
Darien Bernal <darien.bernal@gmail.com>
2025-02-23 23:40:01 -0500

    Changed from then catch to async await

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
-fetch("https://openlibrary.org/search.json?q=harry+potter")
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(error => console.error(error));
+// fetch("https://openlibrary.org/search.json?q=harry+potter")
+//     .then(response => response.json())
+//     .then(data => console.log(data))
+//     .catch(error => console.error(error));
+
+async function fetchData(params) {
+    try {
+        const response = await fetch("https://openlibrary.org/search.json?q=harry+potter");
+
+        if(!response.ok) {
+            throw new Error("Could not find resource!");
+        }
+
+        const data = await response.json();
+        console.log(data);
+    }
+    catch(error) {
+        console.error(error);
+    }
+}
+
+fetchData();
